a29f75d16a7c1915f4283268149c2c1c0e056cb0
anpoliakov <Andrewlybimenko@gmail.com>
2022-11-24 23:05:45 +0300

    add task 2 stupid solution

---
+++ TasksCodewars/src/by/anpoliakov/task2/Runner.java
@@ -0,0 +1,28 @@
+package by.anpoliakov.task2;
+
+/*
+* Подсчёт гласных букв кроме "Y" в переданном тексте
+* */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(getCount("abracadabra"));
+    }
+
+    private static int getCount(String word) {
+        char [] masVowels = {'a','e','i','o','u'};
+        char[] charsWord = word.trim().toLowerCase().toCharArray();
+        int count = 0;
+
+        if(charsWord.length > 0){
+            for(char c : charsWord){
+                for(char b : masVowels){
+                    if(c == b){
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
